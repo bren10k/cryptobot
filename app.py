c683b48f00f73e3c33066dd01a529056fe8fd86d
bren10k <mauibrenton@gmail.com>
2019-01-09 13:52:41 -1000

    Update app.py

--- app.py
+++ app.py
@@ -81,21 +81,60 @@ def makeWebhookResult(data):
     print(speech)
 
     return {
-        "fulfillmentText": speech,
-        "fulfillmentMessages": [
-               {
-                 "text": [
-                   speech
-                  ],
-               }
-                  ],
-        "payload": {
-            "google": {
-                "expect_user_response": "false",
-                 }
-            }
-
+  "fulfillmentText": speech,
+  "fulfillmentMessages": [
+    {
+      "card": {
+        "title": "card title",
+        "subtitle": "card text",
+        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+        "buttons": [
+          {
+            "text": "button text",
+            "postback": "https://assistant.google.com/"
           }
+        ]
+      }
+    }
+  ],
+  "source": "example.com",
+  "payload": {
+    "google": {
+      "expectUserResponse": false,
+      "richResponse": {
+        "items": [
+          {
+            "simpleResponse": {
+              "textToSpeech": "this is a simple response"
+            }
+          }
+        ]
+      }
+    },
+    "facebook": {
+      "text": "Hello, Facebook!"
+    },
+    "slack": {
+      "text": "This is a text response for Slack."
+    }
+  },
+  "outputContexts": [
+    {
+      "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+      "lifespanCount": 5,
+      "parameters": {
+        "param": "param value"
+      }
+    }
+  ],
+  "followupEventInput": {
+    "name": "event name",
+    "languageCode": "en-US",
+    "parameters": {
+      "param": "param value"
+    }
+  }
+}
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
